94ab71953a055937048cb76b2aabf2946e365acb
khoahd7621 <hoangdangkhoa7621@gmail.com>
2022-10-20 09:27:15 +0700

    BE: Config postgre database

--- backend/src/main/java/com/khoahd7621/youngblack/App.java
+++ backend/src/main/java/com/khoahd7621/youngblack/App.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class YoungblackApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(YoungblackApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
