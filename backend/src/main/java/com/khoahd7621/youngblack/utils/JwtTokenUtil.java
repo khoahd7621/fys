c20b86136014e7400837b1b06ebe9cff2dc410a1
khoahd7621 <hoangdangkhoa7621@gmail.com>
2022-10-23 22:27:59 +0700

    BE: Add more functions into JwtTokenUtil

--- backend/src/main/java/com/khoahd7621/youngblack/utils/JwtTokenUtil.java
+++ backend/src/main/java/com/khoahd7621/youngblack/utils/JwtTokenUtil.java
@@ -1,7 +1,9 @@
 package com.khoahd7621.youngblack.utils;
 
 import com.khoahd7621.youngblack.entities.User;
-import com.khoahd7621.youngblack.models.user.TokenPayload;
+import com.khoahd7621.youngblack.models.TokenPayload;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -52,4 +54,48 @@ public class JwtTokenUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + refreshTokenExpiredDate * 1000))
                 .signWith(SignatureAlgorithm.HS256, secretKey).compact();
     }
+
+    public TokenPayload getTokenPayload(String token) {
+        final Claims claims = parseToken(token).getBody();
+        Map<String, Object> mapResult = (Map<String, Object>) claims.get("payload");
+        return TokenPayload.builder()
+                .id(Long.parseLong(mapResult.get("id").toString()))
+                .email(mapResult.get("email").toString())
+                .phone(mapResult.get("phone").toString()).build();
+
+    }
+
+    public boolean validateToken(String token, User user) {
+        TokenPayload tokenPayload = getTokenPayload(token);
+        return tokenPayload.getId() == user.getId() &&
+                tokenPayload.getEmail().equals(user.getEmail()) &&
+                tokenPayload.getPhone().equals(user.getPhone()) &&
+                !isTokenExpired(token);
+    }
+
+    private Jws<Claims> parseToken(String token) {
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        final Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        Date expiredDate = claims.getExpiration();
+        return expiredDate.before(new Date());
+    }
+
+
+//    public TokenPayload getTokenPayload(String token) {
+//        return getClaimsFromToken(token, (Claims claim) -> {
+//            Map<String, Object> mapResult = (Map<String, Object>) claim.get("payload");
+//            return TokenPayload.builder()
+//                    .id((Long) mapResult.get("id"))
+//                    .email((String) mapResult.get("email"))
+//                    .phone((String) mapResult.get("phone")).build();
+//        });
+//    }
+
+//    private <T> T getClaimsFromToken(String token, Function<Claims, T> claimsResolver) {
+//        final Claims claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+//        return claimsResolver.apply(claims);
+//    }
 }
