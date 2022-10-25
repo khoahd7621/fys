bb23c526e05485d32ad38a41b73611165b4f52aa
khoahd7621 <hoangdangkhoa7621@gmail.com>
2022-10-26 00:04:28 +0700

    BE: Add UserDTOLoginResponse

---
+++ backend/src/main/java/com/khoahd7621/youngblack/dtos/response/user/UserDTOLoginResponse.java
@@ -0,0 +1,27 @@
+package com.khoahd7621.youngblack.dtos.response.user;
+
+import com.khoahd7621.youngblack.constants.EAccountStatus;
+import com.khoahd7621.youngblack.constants.ERoles;
+import lombok.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTOLoginResponse {
+    private long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phone;
+    private String address;
+    private ERoles role;
+    private EAccountStatus status;
+    private Date createdAt;
+    private Date updatedAt;
+    private String accessToken;
+    private String refreshToken;
+}
