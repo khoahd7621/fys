6f48aa9af086b7bc21b87a60cc76c9f6d373d0f1
khoahd7621 <hoangdangkhoa7621@gmail.com>
2022-10-27 13:56:46 +0700

    BE: Add function findCategoryByName

--- backend/src/main/java/com/khoahd7621/youngblack/repositories/CategoryRepository.java
+++ backend/src/main/java/com/khoahd7621/youngblack/repositories/CategoryRepository.java
@@ -4,6 +4,11 @@ import com.khoahd7621.youngblack.entities.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    
+    public Optional<Category> findByName(String name);
+
 }
