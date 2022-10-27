1c19aefc58202f38c8dc0aff1ea2d197039bbb49
khoahd7621 <hoangdangkhoa7621@gmail.com>
2022-10-27 21:38:55 +0700

    BE: Remove some unnecessary import

--- backend/src/main/java/com/khoahd7621/youngblack/securities/ExceptionHandlerFilter.java
+++ backend/src/main/java/com/khoahd7621/youngblack/securities/ExceptionHandlerFilter.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -18,8 +17,7 @@ import java.io.IOException;
 public class ExceptionHandlerFilter extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-            throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) {
         try {
             filterChain.doFilter(request, response);
         } catch (Exception ex) {
