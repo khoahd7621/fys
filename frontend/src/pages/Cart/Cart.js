b027d881b43d0334fe27bdbfefe512346fb311bf
khoahd7621 <hoangdangkhoa7621@gmail.com>
2022-11-01 21:03:22 +0700

    FE: Implement show list items; handle increase, decrease quantity, remove item from cart page

--- frontend/src/pages/Cart/Cart.js
+++ frontend/src/pages/Cart/Cart.js
@@ -3,77 +3,163 @@ import classNames from 'classnames/bind';
 import styles from './Cart.module.scss';
 
 import { BreadCrumb } from '~/components';
+import { useDispatch, useSelector } from 'react-redux';
+import { formatVietnamMoney } from '~/utils/format';
+import { deCreaseOneProductInCart, inCreaseOneProductInCart, removeProductFromCart } from '~/redux/slice/cartSlice';
+import { Link } from 'react-router-dom';
+import { publicRoutes } from '~/routes/routes';
 
 const cx = classNames.bind(styles);
 
 const Cart = () => {
+  const cart = useSelector((state) => state.cart.cart);
+  const dispatch = useDispatch();
+
+  const handleInCreaseOneProduct = (product) => {
+    dispatch(
+      inCreaseOneProductInCart({
+        product,
+      }),
+    );
+  };
+
+  const handleDeCreaseOneProduct = (product) => {
+    dispatch(
+      deCreaseOneProductInCart({
+        product,
+      }),
+    );
+  };
+
+  const handleRemoveProductOutOfCart = (product) => {
+    dispatch(
+      removeProductFromCart({
+        product,
+      }),
+    );
+  };
+
+  const totalMoney =
+    cart?.items &&
+    cart?.items.length > 0 &&
+    cart.items.reduce((previous, current) => previous + current.quantity * current.product.product.price, 0);
+  const totalItems = cart?.items?.reduce((previous, current) => previous + current.quantity, 0);
+
   return (
     <div className={cx('cart')}>
       <BreadCrumb current="cart" />
       <div className="container mx-auto max-w-[730px] lg:max-w-[970px] xl:max-w-[1150px] px-3 mb-5">
         <div className={cx('title', 'mt-7')}>
           <h1 className="text-[26px] uppercase">
-            Cart <span className="text-[14px] lowercase">(12 products)</span>
+            Cart <span className="text-[14px] lowercase">({totalItems} products)</span>
           </h1>
         </div>
         <div className="grid grid-cols-12">
-          <div className={cx('list-products', 'col-span-12 lg:col-span-9')}>
-            <div className={cx('product', 'py-4')}>
-              <div className="grid grid-cols-12">
-                <div className="col-span-2 lg:col-span-3">
-                  <div className={cx('image')}>
-                    <img
-                      className="lg:max-h-[180px]"
-                      src={
-                        'https://bizweb.dktcdn.net/thumb/medium/100/331/067/products/308066421-5438382269603243-2253586239615982651-n.jpg'
-                      }
-                      alt="product name"
-                      title="product name"
-                    />
-                  </div>
-                </div>
-                <div className="col-span-10 lg:col-span-9">
-                  <div className={cx('detail flex items-center justify-between lg:justify-start h-full')}>
-                    <div className="infor w-[350px]">
-                      <p className={cx('name', 'text-sm lg:text-base font-semibold uppercase')}>
-                        Multiple personality t-shirt - s / white
-                      </p>
-                      <p className={cx('brands', 'text-sm font-medium text-[#a9a9a9] my-1')}>Brand: YG Shop</p>
-                      <div className="mobile-price block lg:hidden text-sm font-normal">Price: 350.000đ</div>
-                      <p className={cx('action')}>
-                        <button className="hidden lg:block hover:underline">Delete</button>
-                      </p>
-                    </div>
-                    <div className="price hidden lg:block w-[110px] text-lg font-semibold">350.000đ</div>
-                    <div className={cx('quantity-block')}>
-                      <div>
-                        <button className={cx('decrease')}>-</button>
-                        <span>13</span>
-                        <button className={cx('increase')}>+</button>
+          {cart?.items && cart?.items.length === 0 && (
+            <div className="col-span-12">
+              <div className="flex flex-col items-center mb-5">
+                <div className="text-xl my-10">Your cart is empty</div>
+                <Link
+                  to={`${publicRoutes.collection}/all`}
+                  className={cx('bg-black h-[50px] w-[250px] text-white leading-[50px] text-center')}
+                >
+                  Continue shopping
+                </Link>
+              </div>
+            </div>
+          )}
+          {cart?.items && cart?.items.length > 0 && (
+            <>
+              <div className={cx('list-products', 'col-span-12 lg:col-span-9')}>
+                <div className={cx('product', 'py-4')}>
+                  {cart?.items.map((item, index) => (
+                    <div key={`cart-product-${item?.product?.variantSizeId}-${index}`} className="grid grid-cols-12">
+                      <div className="col-span-2 lg:col-span-3">
+                        <div className={cx('image')}>
+                          <img
+                            className="lg:max-h-[180px]"
+                            src={item?.product?.images && item?.product?.images[0]?.imageUrl}
+                            alt={item?.product?.product?.name}
+                            title={item?.product?.product?.name}
+                          />
+                        </div>
                       </div>
-                      <p className="mobile-action block lg:hidden hover:underline text-center mt-2 cursor-pointer">
-                        Delete
-                      </p>
+                      <div className="col-span-10 lg:col-span-9">
+                        <div className={cx('detail flex items-center justify-between lg:justify-start h-full')}>
+                          <div className="infor w-[350px]">
+                            <p className={cx('name', 'text-sm lg:text-base font-semibold uppercase')}>
+                              {`${item?.product?.product?.name} - ${item?.product?.size?.size} / ${item?.product?.color?.name}`}
+                            </p>
+                            <p className={cx('brands', 'text-sm font-medium text-[#a9a9a9] my-1')}>Brand: YG Shop</p>
+                            <div className="mobile-price block lg:hidden text-sm font-normal">
+                              Price:{' '}
+                              {item?.product?.product?.promotion
+                                ? formatVietnamMoney.format(item?.product?.product?.discountPrice)
+                                : formatVietnamMoney.format(item?.product?.product?.price)}
+                            </div>
+                            <p className={cx('action')}>
+                              <button
+                                className="hidden lg:block hover:underline"
+                                onClick={() => handleRemoveProductOutOfCart(item?.product)}
+                              >
+                                Delete
+                              </button>
+                            </p>
+                          </div>
+                          <div className="price hidden lg:block w-[110px] text-lg font-semibold">
+                            {item?.product?.product?.promotion
+                              ? formatVietnamMoney.format(item?.product?.product?.discountPrice)
+                              : formatVietnamMoney.format(item?.product?.product?.price)}
+                          </div>
+                          <div className={cx('quantity-block')}>
+                            <div>
+                              <button
+                                className={cx('decrease')}
+                                onClick={() => handleDeCreaseOneProduct(item?.product)}
+                              >
+                                -
+                              </button>
+                              <span>{item?.quantity}</span>
+                              <button
+                                className={cx('increase')}
+                                onClick={() => handleInCreaseOneProduct(item?.product)}
+                              >
+                                +
+                              </button>
+                            </div>
+                            <p
+                              className="mobile-action block lg:hidden hover:underline text-center mt-2 cursor-pointer"
+                              onClick={() => handleRemoveProductOutOfCart(item?.product)}
+                            >
+                              Delete
+                            </p>
+                          </div>
+                        </div>
+                      </div>
                     </div>
-                  </div>
+                  ))}
                 </div>
               </div>
-            </div>
-          </div>
-          <div className={cx('cart-submit', 'col-span-12 lg:col-span-3')}>
-            <div className={cx('fee')}>
-              <div className={cx('temporarily')}>
-                <span>Temporarily:</span>
-                <span>11.312.000đ</span>
+              <div className={cx('cart-submit', 'col-span-12 lg:col-span-3')}>
+                <div className={cx('fee')}>
+                  <div className={cx('temporarily')}>
+                    <span>Temporarily:</span>
+                    <span>{formatVietnamMoney.format(totalMoney)}</span>
+                  </div>
+                  <div className={cx('amount')}>
+                    <span>Amount:</span>
+                    <span>{formatVietnamMoney.format(totalMoney)}</span>
+                  </div>
+                  <Link to={publicRoutes.checkout} className={cx('checkout-btn')}>
+                    Pay now
+                  </Link>
+                  <Link to={`${publicRoutes.collection}/all`} className={cx('continue-shopping-btn')}>
+                    Continue shopping
+                  </Link>
+                </div>
               </div>
-              <div className={cx('amount')}>
-                <span>Amount:</span>
-                <span>11.312.000đ</span>
-              </div>
-              <button className={cx('checkout-btn')}>Pay now</button>
-              <button className={cx('continue-shopping-btn')}>Continue shopping</button>
-            </div>
-          </div>
+            </>
+          )}
         </div>
       </div>
     </div>
