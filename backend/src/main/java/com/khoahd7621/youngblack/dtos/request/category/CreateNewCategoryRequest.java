29e4350861650649c9bbe74125206cd5e2cd36c7
khoahd7621 <hoangdangkhoa7621@gmail.com>
2022-11-04 09:39:45 +0700

    BE: Hotfix create new category, color and size of product

--- backend/src/main/java/com/khoahd7621/youngblack/dtos/request/category/CreateNewCategoryRequest.java
+++ backend/src/main/java/com/khoahd7621/youngblack/dtos/request/category/CreateNewCategoryRequest.java
@@ -1,14 +1,14 @@
 package com.khoahd7621.youngblack.dtos.request.category;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateNewCategoryRequest {
     @NotBlank(message = "Name of category is required")
     private String name;
--- backend/src/main/java/com/khoahd7621/youngblack/dtos/request/color/CreateNewColorRequest.java
+++ backend/src/main/java/com/khoahd7621/youngblack/dtos/request/color/CreateNewColorRequest.java
@@ -1,14 +1,14 @@
 package com.khoahd7621.youngblack.dtos.request.color;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotEmpty;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateNewColorRequest {
     @NotEmpty(message = "Color name is required")
     private String name;
